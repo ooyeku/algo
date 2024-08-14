fe4ad63459d207980ed6783a086d90407e1d540f
Ola Yeku <ooyeku@gmail.com>
2024-08-13 21:58:50 -0500

    Add early return for empty array in JumpSearch functions
    Improve the JumpSearch and JumpSearchGeneric functions by adding an early return when the input array is empty. This ensures better performance and avoids unnecessary computations. Code indentation was also corrected for consistency.

--- algo/searching/jump_search.go
+++ algo/searching/jump_search.go
@@ -19,6 +19,9 @@ import "math"
 // JumpSearch performs a jump search on a sorted integer slice
 func JumpSearch(arr []int, x int) int {
 	n := len(arr)
+	if n == 0 {
+		return -1 // Return immediately if the array is empty
+	}
 	step := int(math.Sqrt(float64(n)))
 	prev := 0
 
@@ -47,6 +50,9 @@ func JumpSearch(arr []int, x int) int {
 // JumpSearchGeneric performs a jump search on a sorted slice of any comparable type
 func JumpSearchGeneric[T comparable](arr []T, x T, less func(T, T) bool) int {
 	n := len(arr)
+	if n == 0 {
+		return -1 // Return -1 or appropriate value for empty array
+	}
 	step := int(math.Sqrt(float64(n)))
 	prev := 0
 
